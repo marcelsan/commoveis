0923919a1df8bf6582ed202316ffb35faef745ad
Esdras Júnior <eblsj@cin.ufpe.br>
2017-11-01 18:53:47 -0300

    Geodesic distance function added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsRegressor
+from math import acos, sin, cos, radians
 
 neigh = KNeighborsRegressor(n_neighbors=2)
 df = pd.read_csv('medicoes.csv')
@@ -13,4 +14,8 @@ neigh.fit(X, y)
 
 # neigh.predict([[1.5]])
 # Implementar função de distância geodésica (pq a terra é redonda) -> Python.
+geo_dist = lambda a, b: acos(sin(radians(a[0]))*sin(radians(b[0]))+cos(radians(a[0]))*cos(radians(b[0]))*cos(radians(a[1]-b[1]))) * 6378100
 # Usar a função de distância geodésica para calcular erro médio quadrático.
+
+
+
