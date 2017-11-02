853e83ad557e0cee92094a4bd0d5ada3be824bde
Esdras Júnior <eblsj@cin.ufpe.br>
2017-11-02 11:03:32 -0300

    Error metric changed to simple geodesic distance mean

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ neigh.fit(X, y)
 geo_dist = lambda a, b: acos(sin(radians(a[0]))*sin(radians(b[0]))+cos(radians(a[0]))*cos(radians(b[0]))*cos(radians(a[1]-b[1]))) * 6378100
 # Usar a função de distância geodésica para calcular erro médio quadrático.
 dist_err = np.array(list(map(lambda x: geo_dist(x[0], x[1]), zip(neigh.predict(X_test), y_test))))
-sqr_mean = np.sqrt(np.mean(dist_err*dist_err))
+sqr_mean = (np.mean(dist_err))
 print(sqr_mean)
 
