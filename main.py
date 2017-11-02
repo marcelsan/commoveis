669cc8e8b68a3a9c2a9d7fdccf16020e406d1cd6
Esdras Júnior <eblsj@cin.ufpe.br>
2017-11-02 10:49:57 -0300

    Temporary test partition added; Mean squared error calculated with geodesic distance

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ df = pd.read_csv('medicoes.csv')
 df = shuffle(df)
 X = df.iloc[:, 2:].values
 y = df.iloc[:, 0:2].values
-neigh.fit(X, y)
 
+# temporary test
+part = int(len(X)/5)
+X_test = X[:part]
+y_test = y[:part]
+X = X[part:]
+y = y[part:]
+
+neigh.fit(X, y)
 
 # neigh.predict([[1.5]])
 # Implementar função de distância geodésica (pq a terra é redonda) -> Python.
 geo_dist = lambda a, b: acos(sin(radians(a[0]))*sin(radians(b[0]))+cos(radians(a[0]))*cos(radians(b[0]))*cos(radians(a[1]-b[1]))) * 6378100
 # Usar a função de distância geodésica para calcular erro médio quadrático.
-
-
+dist_err = np.array(list(map(lambda x: geo_dist(x[0], x[1]), zip(neigh.predict(X_test), y_test))))
+sqr_mean = np.sqrt(np.mean(dist_err*dist_err))
+print(sqr_mean)
 
